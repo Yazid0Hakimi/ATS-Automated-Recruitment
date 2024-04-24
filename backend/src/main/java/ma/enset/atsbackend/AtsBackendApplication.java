b369abd2fe368db4f9aafa82486274c7c540931a
Yazid0Hakimi <71094777+Yazid0Hakimi@users.noreply.github.com>
2024-04-24 21:37:22 +0100

    setting backend

---
+++ backend/src/main/java/ma/enset/atsbackend/AtsBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.atsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtsBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ma/enset/atsbackend/AtsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.atsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
