3d3ca17102e99cf650283fedb457481ad2dcfd78
wail-10 <koundiwail02@gmail.com>
2024-05-17 20:12:04 +0000

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,14 +9,21 @@ const Navbar = () => {
                 <Link to="/" className='font-black text-xl'>Logo</Link>
             </div>
             <div className='flex justify-center items-center'>
+                <Link to='/' className='font-semibold p-2 hover:text-[#2D82B7]'>Home</Link>
+                <Link to='/home' className='font-semibold p-2 hover:text-[#2D82B7]'>Find Jobs</Link>
+                <Link to='/login' className='bg-transparent border border-[#2D82B7] font-semibold text-[#2D82B7] mx-1 px-4 py-2 rounded-md'>Sign In</Link>
+                <Link to='/register' className='bg-[#2D82B7] border border-[#2D82B7] font-semibold text-white mx-1 px-4 py-2 rounded-md'>Sign Up</Link>
+            </div>
+            {/* Shown When a user is connected */}
+            {/* <div className='flex justify-center items-center'>
                 <button 
                 type='button'
                 onClick={() => setShowNav(!showNav)}
                 >
                     <p>Username</p>
                 </button>
-            </div>
-            {showNav && (
+            </div> */}
+            {/* {showNav && (
                 <div className="absolute z-50 right-2 top-20 w-[200px] bg-white border border-[#7A867A] shadow-md rounded-md p-2">
                     <Link to="/" className='flex items-center w-full text-left p-2 transition hover:bg-[#000] hover:text-white'>
                         Profile
@@ -28,7 +35,7 @@ const Navbar = () => {
                         Logout
                     </button>
                 </div>
-            )}
+            )} */}
         </nav>
     )
 }
